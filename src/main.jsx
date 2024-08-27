5120c2bb6561333bb44b127425439aa72e2b8553
michaellgans <michaellgans@Victoria>
2024-08-27 12:09:03 -0500

    MLG - 12:08 PM - updates main.jsx to render react to index.html

--- src/main.jsx
+++ src/main.jsx
@@ -1,3 +1,10 @@
+// Entry point into the React App: index.html
+
 import "./index.css";
+import ReactDOM from "react-dom/client";
+import App from "./App";
 
 // Render the App component to the root element
+const div = document.getElementById("root");
+const root = ReactDOM.createRoot(div);
+root.render(<App />);
