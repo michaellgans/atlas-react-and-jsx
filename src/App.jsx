679fc1df6ece601c8ffe0502b6eb02aa74bf192d
michaellgans <michaellgans@Victoria>
2024-08-27 15:44:13 -0500

    MLG - 3:43 PM - nests the Greeting component inside App

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,11 @@
+import { Greeting } from "./components/Greeting";
+
 function App() {
-  return <div className="app">Hello Atlas</div>;
+  return (
+    <div className="app">
+      <Greeting />
+    </div>
+  );
 }
 
 export default App;
