51fcf9608c5c7898b103d52b746398c222b6d5b8
michaellgans <michaellgans@Victoria>
2024-08-28 13:09:09 -0500

    MLG - 1:08 PM - updates the Section Component so that it renders x children

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,30 @@
 import { Header } from "./components/Header";
+import { Section } from "./components/Section";
 
 function App() {
   return (
     <div className="app">
       <Header />
+      <Section title="What is react?">
+        <p>
+          React is a JavaScript library that was developed by Facebook in 2013.
+          Its main purpose is to empower developers build user interfaces (UI's)
+          through the creation of components - or modularized chunks of HTML -
+          that are then rendered to the DOM through JavaScript functions. 
+          Each function typically returns a JSX representation of what should be
+          rendered to the DOM, or more simply put, displayed on the web page.
+          Generally speaking, these elements are simple with minimal styling
+          because they need to be able to be reuseable, like a button.
+          From there, the elements can be customized through the use of properties.
+        </p>
+        <a
+          href="https://www.linkedin.com/pulse/reacting-react-virtual-vs-actual-dom-michael-l-gans-jpbhc/?trackingId=O6gx1yVdQJiNgXHPY8TJCg%3D%3D"
+          aria-label="Reacting to React Article"
+          target="_blank"
+        >
+          <cite>Reacting to React: Virtual vs Actual DOM</cite>
+        </a>
+      </Section>
     </div>
   );
 }
--- src/components/Section.jsx
+++ src/components/Section.jsx
@@ -1,20 +1,15 @@
 // Section Component
 
-// Asset Imports
-
-// Script Imports
+import { Children } from "react";
 
 // Returns a Section Component
-export function Section({title}) {
+export function Section({title, children}) {
     return (
         <div className="section">
             <h1>{title}</h1>
-            <p>
-                React is not a language of its own; rather, it is a JavaScript library that was developed by Facebook in 2013. Its main purpose is to empower developers build user interfaces (UI's) through the creation of components - or modularized chunks of HTML - that are then rendered to the DOM through JavaScript functions. 
-                Each function typically returns a JSX representation of what should be rendered to the DOM, or more simply put, displayed on the web page. Generally speaking, these elements are simple with minimal styling because they need to be able to be reuseable, like a button. From there, the elements can be customized through the use of properties (React calls them props). 
-                An example of this is if I have 14 buttons on my UI instead of manually coding all 14, I can have one "button" function that returns a very general, simple button. I can then use that button where I need, and style it differently when necessary. Less coding, more modularity.
-                This is a unique approach to web development because you often have one HTML document, typically called "index.html", instead of multiple pages that need to be navigated between. React allows you to build a single-page application (SPA) that only updates the DOM when and where a change is needed instead of updating the entire DOM every time any change happens.
-            </p>
+            {Children.map(children, child =>
+                child
+            )}
         </div>
     );
 }
