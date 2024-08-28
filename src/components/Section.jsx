147e13f6ea8e1dee6513618af9d9b68594e96097
michaellgans <michaellgans@Victoria>
2024-08-28 17:15:23 -0500

    MLG - 5:14 PM - simplifies Section logic

--- src/components/Section.jsx
+++ src/components/Section.jsx
@@ -1,15 +1,11 @@
 // Section Component
 
-import { Children } from "react";
-
 // Returns a Section Component
 export function Section({title, children}) {
     return (
         <div className="section">
             <h1>{title}</h1>
-            {Children.map(children, child =>
-                child
-            )}
+            {children}
         </div>
     );
 }
